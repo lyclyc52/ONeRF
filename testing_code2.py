b76c8e7dbef4b5d876b43fb446086506b5f37eb2
LIANG-SN <liangsn2001@outlook.com>
2021-09-12 12:36:45 +0800

    update training

--- testing_code2.py
+++ testing_code2.py
@@ -1,12 +1,20 @@
-from models import *
-from train import *
+# from models import *
+# from train import *
+import numpy as np
+import tensorflow as tf
+import os
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+os.environ["CUDA_VISIBLE_DEVICES"]="8"
+from multi_object_datasets import multi_dsprites
 
-hwf = (400, 400, 400)
+data_path = '/data/sliangal/clevr_with_masks_clevr_with_masks_train.tfrecords'
 
-# fn = Encoder_Decoder(cam_param=hwf, num_slots=5, num_iterations=3, resolution=(400, 400), 
-#     decoder_initial_size=(25, 25))
-
-images = tf.ones((10, 400, 400, 3))
-depth_maps = tf.ones((10, 400, 400))
-poses = tf.ones((10, 4, 4))
-train(images, depth_maps, hwf, poses, num_slots=5, num_iters=3)
+batch_size = 1
+dataset = multi_dsprites.dataset(data_path, 'colored_on_colored')
+# batched_dataset = dataset.batch(batch_size)  # optional batching
+# iterator = batched_dataset.make_one_shot_iterator()
+# data = iterator.get_next()
+dataset = dataset.enumerate(start=0)
+for element in dataset:
+  print(element)
+exit()
--- train.py
+++ train.py
@@ -6,52 +6,75 @@ from models import *
 from run_nerf import *
 
 
-def save_weights(net, i):
+def save_weights_npy(net, i):
     path = os.path.join(
         'weights', '{:06d}.npy'.format(i))
     np.save(path, net.get_weights())
     print('saved weights at', path)
 
-def train(images, depth_maps, hwf, poses, num_slots, num_iters,
-        N_save=100, N_img=100, train_iters=100000):
+def load_weights_npy(dir, model):
+    ckpts = [os.path.join(dir, f) for f in sorted(os.listdir(os.path.join(dir)))]
+    if len(ckpts) > 0:
+        ckpt = ckpts[-1]
+        print('reload from: ', ckpt)
+        model.set_weights(np.load(ckpt, allow_pickle=True))
+
+def train(images, depth_maps, hwf, poses, num_slots, num_iters=3,
+        N_save=100, N_img=100, train_iters=100000, lrate=1e-4):
     # images: (N, H, W, C)
     # depth_maps : (N, H, W)
     # poses: (N, 4, 4)
 
-    model = build_model(hwf, num_slots, num_iters, data_shape=images.shape)
-    trainable_vars = model.trainable_weights
+    os.makedirs('./imgs', exist_ok=True)
+    os.makedirs('./checkpoints', exist_ok=True)
 
-    lrate = 1e-4
+    model = build_model(hwf, num_slots, num_iters, data_shape=images[0:1].shape)
+    trainable_vars = model.trainable_weights
     # if args.lrate_decay > 0:
     #     lrate = tf.keras.optimizers.schedules.ExponentialDecay(lrate,
     #                                                            decay_steps=args.lrate_decay * 1000, decay_rate=0.1)
     optimizer = tf.keras.optimizers.Adam(lrate)
 
-    for i in range(train_iters):
-        with tf.GradientTape() as tape:
-            recon, rgbs, masks, slots = model([images, depth_maps, poses])
+    ckpt = tf.train.Checkpoint(
+        network=model, optimizer=optimizer)
+    ckpt_manager = tf.train.CheckpointManager(
+        checkpoint=ckpt, directory='./checkpoints', max_to_keep=None)
+    ckpt.restore(ckpt_manager.latest_checkpoint)
+    # load_weights_npy('./weights', model)
 
-            loss = img2mse(recon, images)
+    N_imgs = images.shape[0]
+
+    for i in range(20001, train_iters):
+        t = np.random.randint(0, N_imgs)
+        with tf.GradientTape() as tape:
+            recon, rgbs, masks, slots = model([images[t:t+1], depth_maps[t:t+1], poses[t:t+1]])
+
+            loss = img2mse(recon, images[t])
         
         gradients = tape.gradient(loss, trainable_vars)
         optimizer.apply_gradients(zip(gradients, trainable_vars))
 
-        print('iter', i)
+        print('iter: {:06d}, img: {:03d}, loss: {:f}'.format(i, t, loss))
 
         if i % N_save == 0:
-            save_weights(model, i)
+            # save_weights_npy(model, i)
+            ckpt_manager.save(i)
         
         if i % N_img == 0:
-            imageio.imwrite(os.path.join('./imgs/{:06d}.jpg'.format(i)), recon)
+            imageio.imwrite(os.path.join('./imgs/{:06d}.jpg'.format(i)), to8b(recon))
             for j in range(rgbs.shape[0]):
                 imageio.imwrite(os.path.join(
-                    './imgs/{:06d}_{:03d}.jpg'.format(i, j)), rgbs[j])
+                    './imgs/{:06d}_{:03d}_{:03d}.jpg'.format(i, j, t)), to8b(rgbs[j]))
+        if i == 0:
+            for j in range(images.shape[0]):
+                imageio.imwrite(os.path.join('./imgs/GT_{:03d}.jpg'.format(j)), images[j])
 
 def main():
     # train args
-    N_save = 100
-    N_img = 100
-    train_iters = 10000
+    N_save = 200
+    N_img = 20
+    train_iters = 50000
+    lrate = 5e-4
     # train args end
 
     depth_maps = np.load('depth.npy')
@@ -59,12 +82,16 @@ def main():
     parser = config_parser()
     args = parser.parse_args()
 
-    N_train = depth_maps.shape[0]
+    # N_train = depth_maps.shape[0] - 10
+    N_train = 20
 
     images, poses, render_poses, hwf, i_split = load_blender_data(
                 args.datadir, args.half_res, args.testskip)
 
-    train(images[0:N_train, :, :, 0:3], depth_maps, hwf, poses[0:N_train], 2, 3, N_save, N_img=N_img, train_iters=train_iters)
+    train(images[0:N_train, :, :, 0:3], depth_maps[0:N_train], hwf, 
+            poses[0:N_train], 3, 3, N_save, N_img=N_img, train_iters=train_iters,
+            lrate=lrate)
 
 if __name__ == '__main__':
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
     main()
